03935b4844c799ffd39aa4f885b406069a41b450
Cassiana <lima.cglima@gmail.com>
2023-05-19 23:57:02 -0300

    feat: Definindo altura e largura de widgets individuais com Kivy

--- input.py
+++ input.py
@@ -14,13 +14,22 @@ class MyGridLayout(GridLayout):
 
         # Set columns
         self.cols = 1
+        self.row_force_default = True
+        self.row_default_height = 120
+        self.col_force_default = True
+        self.col_default_width = 100
 
         # Create a second gridlayout
-        self.top_grid = GridLayout()
+        self.top_grid = GridLayout(row_force_default=True,
+                                   row_default_height=40,
+                                   col_force_default=True,
+                                   col_default_width=100)
+        # Set number of columns in our new topgrid
         self.top_grid.cols = 2
 
         # Add widgets
         self.top_grid.add_widget(Label(text="Name: "))
+
         # Add Input Box
         self.name = TextInput(multiline=True)
         self.top_grid.add_widget(self.name)
@@ -39,7 +48,13 @@ class MyGridLayout(GridLayout):
         self.add_widget(self.top_grid)
 
         # Create a Submit Button
-        self.submit = Button(text="Submit", font_size=32)
+        self.submit = Button(text="Submit",
+                             font_size=32,
+                             size_hint_y=None,
+                             height=50,
+                             size_hint_x=None,
+                             width=200
+                             )
         # Bind the button
         self.submit.bind(on_press=self.press)
         self.add_widget(self.submit)
