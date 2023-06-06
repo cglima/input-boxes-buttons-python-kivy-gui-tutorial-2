069bdda329711a38b3fb7081265eb38c7d4009e8
Cassiana <lima.cglima@gmail.com>
2023-06-06 11:21:52 -0300

    feat: Como usar Radio buttons - permite escolher apenas um elemento da lista - adicionando um grupo nomeado

---
+++ radio_buttons.py
@@ -0,0 +1,35 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+
+
+# Designate Our .kv design file
+Builder.load_file('radio_buttons.kv')
+
+
+class MyLayout(Widget):
+    checks = []
+
+    def checkbox_click(self, instance, value, topping):
+        if value == True:
+            MyLayout.checks.append(topping)
+            tops = ''
+            for x in MyLayout.checks:
+                tops = f'{tops} {x}'
+            self.ids.output_label.text = f'You selected:{tops}'
+        else:
+            MyLayout.checks.remove(topping)
+            tops = ''
+            for x in MyLayout.checks:
+                tops = f'{tops} {x}'
+            self.ids.output_label.text = f'You selected:{tops}'
+            # self.ids.output_label.text = f'You selected:{MyLayout.checks}'
+
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    AwesomeApp().run()
