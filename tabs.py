b71565ab634e23d9c9485f9184c8a3080456ba88
Cassiana <lima.cglima@gmail.com>
2023-06-06 22:24:51 -0300

    feat: como criar tabs in kivy - colocando coisas nas tabs - dimensionando as tabs

---
+++ tabs.py
@@ -0,0 +1,20 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.tabbedpanel import TabbedPanel
+
+# Designate Our .kv design file
+Builder.load_file('tabs.kv')
+
+
+class MyLayout(TabbedPanel):
+    pass
+
+
+class AwesomeApp(App):
+    def build(self):
+        return MyLayout()
+
+
+if __name__ == '__main__':
+    AwesomeApp().run()
