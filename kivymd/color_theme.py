1f2bacd99f7eeb841677684aa68252619d823dbc
Cassiana <lima.cglima@gmail.com>
2023-06-07 22:20:07 -0300

    feat: usando cores em um tema

--- kivymd/color_theme.py
+++ kivymd/color_theme.py
@@ -1,9 +1,16 @@
 from kivy.lang import Builder
 from kivymd.app import MDApp
 
+# 'Red', 'Pink', 'Purple', 'DeepPurple', 'Indigo', 'Blue', 'LightBlue',
+# 'Cyan', 'Teal', 'Green', 'LightGreen', 'Lime', 'Yellow', 'Amber', 'Orange',
+# 'DeepOrange', 'Brown', 'Gray', 'BlueGray'.
+
 
 class MainApp(MDApp):
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "LightGreen"
+        self.theme_cls.accent_palette = "Red"
         return Builder.load_file('color_theme.kv')
 
 
